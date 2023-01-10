9dbee468216c35fd4a5e6b18847bbd44be0e7b28
henry7079 <103790770+henry7079@users.noreply.github.com>
2023-01-09 18:13:36 -0600

    i just created a stored procedure that receive a parameter if it's zero it's goin to return recover even on the contrary odd

--- dateM.sql
+++ dateM.sql
@@ -1,1 +1,17 @@
 SELECT * FROM Employee WHERE DAY(birthday) % 2 = 1; 
+
+ALTER PROCEDURE sp_date
+	@typeDay int
+AS
+BEGIN
+	IF(@typeDay = 0 OR @typeDay = 1)
+		BEGIN
+			SELECT * FROM Employee WHERE DAY(birthday) % 2 = @typeDay AND DATEDIFF(DAY, '1997-01-01', birthday) >= 3;
+		END
+	ELSE
+		BEGIN
+			SELECT 'Error: happened an error' AS error;
+		END
+END
+
+EXEC sp_date 0;
